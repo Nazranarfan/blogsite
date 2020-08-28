6a2a50256a37008b63e18074272964ce004699ee
nazranarfan <nazran.arfanuddin@gmail.com>
2020-08-28 23:58:31 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require 'head.php';?>
+<body>
+    <?php require 'nav.php'; ?>
+        <div class="jumbotron">
+        <h1 class="text-center">Hello World</h1>
+        </div>
+    <?php require 'footer.php'; ?>
+</body>
